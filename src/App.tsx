bc60343b882dccf08726be3ceaabea8c95072017
JulianOviedo <julianovie234@gmail.com>
2023-07-06 17:14:53 -0300

    home

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold  text-center p-4">DIVIDER</h1>
+      <h3 className="text-center mt-8">Lets get started !</h3>
+      <div className="absolute top-2/4 w-full flex justify-center">
+      <button className="rounded-3xl bg-white text-orange-400 py-4 px-20 text-sm">New Join</button>
+      </div>
     </>
   )
 }
